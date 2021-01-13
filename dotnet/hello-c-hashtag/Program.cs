e2062d828324983b7eb6045aa666219174c3518b
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2021-01-13 16:01:16 +0500

    cleaned old code and added hello C#

---
+++ dotnet/hello-c-hashtag/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_c_hashtag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! C HashTag. C# users dont mind please.");
+        }
+    }
+}
