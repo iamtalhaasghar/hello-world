54b20bfcbd37e019cb84731acf33914e6f7038a0
Talha Asghar <talhaasghar220@gmail.com>
2023-04-08 17:43:32 +0500

    misc changes

--- jpython/fx.py
+++ jpython/fx.py
@@ -5,6 +5,7 @@ from javafx.scene import Scene
 from javafx.scene.layout import FlowPane, GridPane
 from javafx.stage import Stage
 import javafx.scene.control
+#from javafx.scene import control
 
 from javafx.geometry import Pos
 
@@ -13,16 +14,16 @@ class HelloApplication(Application):
     
     def start(self, stage):
         root = GridPane()
-        l = javafx.scene.control.Label("Talha")
-        b = javafx.scene.control.Button("Press Me!")
+        #l = javafx.scene.control.Label("Talha")
+        #b = javafx.scene.control.Button("Press Me!")
         root.setAlignment(Pos.CENTER)
-        root.addRow(0,l)
-        root.addRow(1,b)
+        #root.addRow(0,l)
+        #root.addRow(1,b)
         scene = Scene( root, 640, 480 )
         stage.setScene( scene )
         stage.setTitle( "Hello, World" )
         stage.show()
-        b.setOnAction (self.buttonPressed)
+        #b.setOnAction (self.buttonPressed)
     def buttonPressed(self,ae):
        print('Button was pressed.')
 if __name__ == "__main__":
